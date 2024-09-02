53a8fcf0578a6eb07727fecfa8ce4f51eed72d00
vhndaree <ramkumar.bhandari@rewe-digital.com>
2024-09-03 00:33:08 +0200

    implement basic blockchain prototype

---
+++ blockchain.go
@@ -0,0 +1,16 @@
+package aagyalink
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func NewBlockChain() *Blockchain {
+	genesisBlock := NewBlock("Genesis Block!", []byte{})
+	return &Blockchain{blocks: []*Block{genesisBlock}}
+}
+
+func (bc *Blockchain) AddBlocks(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aagyalink
+
+go 1.22.5
